e7382cc3708efcc5a1e35809095981b1c257e199
Shrimpu <joar.andersson@elev.ga.lbs.se>
2019-05-25 19:52:32 +0200

    added a singular comment to the winner of the most ass script of the project. enjoy

--- Assets/Scripts/UnPurchasedUnit.cs
+++ Assets/Scripts/UnPurchasedUnit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New UnitPreview", menuName = "UnitPreview")]
-public class UnPurchasedUnit : ScriptableObject
+public class UnPurchasedUnit : ScriptableObject // this script holds what youll see when draging a unit from the shop and the unit it represents
 {
     [Tooltip("The Unit it represents")]
     public GameObject unit;
